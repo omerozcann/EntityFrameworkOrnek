f357d68944b9b387080d09d297fbf6c4e0d262eb
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2021-12-22 17:27:52 +0300

    .

---
+++ EntityOrnek/EntityOrnek/entityFrm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace EntityOrnek
+{
+    partial class entityFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aditxt = new System.Windows.Forms.TextBox();
+            this.soyaditxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sehirtxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.adresrtb = new System.Windows.Forms.RichTextBox();
+            this.kaydetbtn = new System.Windows.Forms.Button();
+            this.silbtn = new System.Windows.Forms.Button();
+            this.guncellebtn = new System.Windows.Forms.Button();
+            this.temizlebtn = new System.Windows.Forms.Button();
+            this.tablegrd = new System.Windows.Forms.DataGridView();
+            this.kayitDataSet = new EntityOrnek.KayitDataSet();
+            this.musteriBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.musteriTableAdapter = new EntityOrnek.KayitDataSetTableAdapters.musteriTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sehirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tablegrd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteriBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı";
+            // 
+            // aditxt
+            // 
+            this.aditxt.Location = new System.Drawing.Point(68, 25);
+            this.aditxt.Name = "aditxt";
+            this.aditxt.Size = new System.Drawing.Size(156, 20);
+            this.aditxt.TabIndex = 1;
+            // 
+            // soyaditxt
+            // 
+            this.soyaditxt.Location = new System.Drawing.Point(68, 51);
+            this.soyaditxt.Name = "soyaditxt";
+            this.soyaditxt.Size = new System.Drawing.Size(156, 20);
+            this.soyaditxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyadı";
+            // 
+            // sehirtxt
+            // 
+            this.sehirtxt.Location = new System.Drawing.Point(68, 77);
+            this.sehirtxt.Name = "sehirtxt";
+            this.sehirtxt.Size = new System.Drawing.Size(156, 20);
+            this.sehirtxt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Şehir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Adres";
+            // 
+            // adresrtb
+            // 
+            this.adresrtb.Location = new System.Drawing.Point(68, 103);
+            this.adresrtb.Name = "adresrtb";
+            this.adresrtb.Size = new System.Drawing.Size(156, 96);
+            this.adresrtb.TabIndex = 7;
+            this.adresrtb.Text = "";
+            // 
+            // kaydetbtn
+            // 
+            this.kaydetbtn.Location = new System.Drawing.Point(68, 214);
+            this.kaydetbtn.Name = "kaydetbtn";
+            this.kaydetbtn.Size = new System.Drawing.Size(75, 23);
+            this.kaydetbtn.TabIndex = 8;
+            this.kaydetbtn.Text = "Kaydet";
+            this.kaydetbtn.UseVisualStyleBackColor = true;
+            this.kaydetbtn.Click += new System.EventHandler(this.kaydetbtn_Click);
+            // 
+            // silbtn
+            // 
+            this.silbtn.Location = new System.Drawing.Point(149, 214);
+            this.silbtn.Name = "silbtn";
+            this.silbtn.Size = new System.Drawing.Size(75, 23);
+            this.silbtn.TabIndex = 9;
+            this.silbtn.Text = "Sil";
+            this.silbtn.UseVisualStyleBackColor = true;
+            this.silbtn.Click += new System.EventHandler(this.silbtn_Click);
+            // 
+            // guncellebtn
+            // 
+            this.guncellebtn.Location = new System.Drawing.Point(68, 243);
+            this.guncellebtn.Name = "guncellebtn";
+            this.guncellebtn.Size = new System.Drawing.Size(75, 23);
+            this.guncellebtn.TabIndex = 10;
+            this.guncellebtn.Text = "Güncelle";
+            this.guncellebtn.UseVisualStyleBackColor = true;
+            this.guncellebtn.Click += new System.EventHandler(this.guncellebtn_Click);
+            // 
+            // temizlebtn
+            // 
+            this.temizlebtn.Location = new System.Drawing.Point(149, 243);
+            this.temizlebtn.Name = "temizlebtn";
+            this.temizlebtn.Size = new System.Drawing.Size(75, 23);
+            this.temizlebtn.TabIndex = 11;
+            this.temizlebtn.Text = "Temizle";
+            this.temizlebtn.UseVisualStyleBackColor = true;
+            this.temizlebtn.Click += new System.EventHandler(this.temizlebtn_Click);
+            // 
+            // tablegrd
+            // 
+            this.tablegrd.AutoGenerateColumns = false;
+            this.tablegrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablegrd.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.adiDataGridViewTextBoxColumn,
+            this.soyadiDataGridViewTextBoxColumn,
+            this.sehirDataGridViewTextBoxColumn,
+            this.adresDataGridViewTextBoxColumn});
+            this.tablegrd.DataSource = this.musteriBindingSource;
+            this.tablegrd.Location = new System.Drawing.Point(247, 12);
+            this.tablegrd.MultiSelect = false;
+            this.tablegrd.Name = "tablegrd";
+            this.tablegrd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tablegrd.Size = new System.Drawing.Size(408, 241);
+            this.tablegrd.TabIndex = 12;
+            this.tablegrd.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tablegrd_CellContentClick);
+            this.tablegrd.Click += new System.EventHandler(this.tablegrd_Click);
+            // 
+            // kayitDataSet
+            // 
+            this.kayitDataSet.DataSetName = "KayitDataSet";
+            this.kayitDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // musteriBindingSource
+            // 
+            this.musteriBindingSource.DataMember = "musteri";
+            this.musteriBindingSource.DataSource = this.kayitDataSet;
+            // 
+            // musteriTableAdapter
+            // 
+            this.musteriTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // adiDataGridViewTextBoxColumn
+            // 
+            this.adiDataGridViewTextBoxColumn.DataPropertyName = "adi";
+            this.adiDataGridViewTextBoxColumn.HeaderText = "adi";
+            this.adiDataGridViewTextBoxColumn.Name = "adiDataGridViewTextBoxColumn";
+            // 
+            // soyadiDataGridViewTextBoxColumn
+            // 
+            this.soyadiDataGridViewTextBoxColumn.DataPropertyName = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.HeaderText = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.Name = "soyadiDataGridViewTextBoxColumn";
+            // 
+            // sehirDataGridViewTextBoxColumn
+            // 
+            this.sehirDataGridViewTextBoxColumn.DataPropertyName = "sehir";
+            this.sehirDataGridViewTextBoxColumn.HeaderText = "sehir";
+            this.sehirDataGridViewTextBoxColumn.Name = "sehirDataGridViewTextBoxColumn";
+            // 
+            // adresDataGridViewTextBoxColumn
+            // 
+            this.adresDataGridViewTextBoxColumn.DataPropertyName = "adres";
+            this.adresDataGridViewTextBoxColumn.HeaderText = "adres";
+            this.adresDataGridViewTextBoxColumn.Name = "adresDataGridViewTextBoxColumn";
+            // 
+            // entityFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 294);
+            this.Controls.Add(this.tablegrd);
+            this.Controls.Add(this.temizlebtn);
+            this.Controls.Add(this.guncellebtn);
+            this.Controls.Add(this.silbtn);
+            this.Controls.Add(this.kaydetbtn);
+            this.Controls.Add(this.adresrtb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sehirtxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.soyaditxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.aditxt);
+            this.Controls.Add(this.label1);
+            this.Name = "entityFrm";
+            this.Text = "Entity Örnek";
+            this.Load += new System.EventHandler(this.entityFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablegrd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteriBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox aditxt;
+        private System.Windows.Forms.TextBox soyaditxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sehirtxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox adresrtb;
+        private System.Windows.Forms.Button kaydetbtn;
+        private System.Windows.Forms.Button silbtn;
+        private System.Windows.Forms.Button guncellebtn;
+        private System.Windows.Forms.Button temizlebtn;
+        private System.Windows.Forms.DataGridView tablegrd;
+        private System.Windows.Forms.BindingSource musteriBindingSource;
+        private KayitDataSetTableAdapters.musteriTableAdapter musteriTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sehirDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adresDataGridViewTextBoxColumn;
+        private KayitDataSet kayitDataSet;
+    }
+}
+
---
+++ EntityOrnek/EntityOrnek/entityFrm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace EntityOrnek
+{
+    public partial class entityFrm : Form
+    {
+        musteri model = new musteri();
+        KayitEntities db = new KayitEntities();
+        BindingSource bs = new BindingSource();
+        public entityFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void kaydetbtn_Click(object sender, EventArgs e)
+        {
+            model.adi = aditxt.Text;
+            model.soyadi = soyaditxt.Text;
+            model.sehir = sehirtxt.Text;
+            model.adres = adresrtb.Text;
+            db.musteri.Add(model);
+            db.SaveChanges();
+            bs.DataSource = db.musteri.ToList();
+            tablegrd.DataSource = bs;
+            
+   
+        }
+
+        private void temizlebtn_Click(object sender, EventArgs e)
+        {
+            aditxt.Clear();
+            soyaditxt.Clear();
+            sehirtxt.Clear();
+            adresrtb.Clear();
+        }
+
+        private void silbtn_Click(object sender, EventArgs e)
+        {
+            model = db.musteri.Find(tablegrd.Rows[tablegrd.SelectedRows[0].Index].Cells[0].Value);
+            db.musteri.Remove(model);
+            db.SaveChanges();
+            bs.DataSource = db.musteri.ToList();
+            tablegrd.DataSource = bs;
+        }
+
+        private void entityFrm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'kayitDataSet.musteri' table. You can move, or remove it, as needed.
+            this.musteriTableAdapter.Fill(this.kayitDataSet.musteri);
+
+        }
+
+        private void guncellebtn_Click(object sender, EventArgs e)
+        {
+            model.adi = aditxt.Text;
+            model.soyadi = soyaditxt.Text;
+            model.sehir = sehirtxt.Text;
+            model.adres = adresrtb.Text;
+            db.Entry(model).State = EntityState.Modified;
+            db.SaveChanges();
+            bs.DataSource = db.musteri.ToList();
+            tablegrd.DataSource = bs;
+        }
+
+        private void tablegrd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void tablegrd_Click(object sender, EventArgs e)
+        {
+            model = db.musteri.Find(tablegrd.Rows[tablegrd.SelectedRows[0].Index].Cells[0].Value);
+            aditxt.Text = model.adi;
+            soyaditxt.Text = model.soyadi;
+            sehirtxt.Text = model.sehir;
+            adresrtb.Text = model.adres;
+
+        }
+    }
+}
